bf6f94320833402fc5ba195d5b48965959f87aa5
Bashar Alwarad <beelwarad52@gmail.com>
2024-11-07 13:43:07 +0100

    102

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,9 +1,8 @@
 import Login from './Login';
 import Home from './Home';
-import Products from './Products';
 import Posts from './Posts';
 import PostDetail from './PostDetail';
 import Users from './Users';
 import Register from './Register';
 
-export { Login, Products, Posts, PostDetail, Home, Users, Register };
+export { Login, Posts, PostDetail, Home, Users, Register };
